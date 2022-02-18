956bc207f400f028dc12334b2fcb5044476e0416
Sabrina <sabrina.mercuri@etu.unice.fr>
2022-02-18 15:37:02 +0100

    bug eureka

---
+++ Eureka-server/src/main/java/com/workshopmicroservices/example/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.workshopmicroservices.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+
+// enable eureka and make it admin
+@EnableEurekaServer
+@EnableAdminServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
--- show-weather/src/main/java/com/example/show/SpringWeatherServiceApplication.java
+++ show-weather/src/main/java/com/example/show/SpringWeatherServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 // we gonna use a hystrix circuit breaker
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class SpringWeatherServiceApplication {
 
 	public static void main(String[] args) {
